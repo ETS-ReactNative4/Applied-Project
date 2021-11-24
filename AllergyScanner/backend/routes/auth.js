efcb63e1d6c0698eda55956c9824d8fdc7748322
Gareth O'Leary <G00375321@gmit.ie>
2021-11-24 17:47:48 +0000

    Created api for login user

--- AllergyScanner/backend/routes/auth.js
+++ AllergyScanner/backend/routes/auth.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 // server side validation
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 
 //Secret key stored on the server
 var jwtSecret = "secrettoken";
@@ -73,7 +74,7 @@ router.post("/", [
 // @route   GET /users/auth
 // @desc    Loading user
 // @access  Private
-router.get("/auth", async (req, res) => {
+router.get("/auth", auth,async (req, res) => {
     try {
         // gets user by token
         const user = await User.findById(req.user.id).select("-password");
@@ -83,5 +84,60 @@ router.get("/auth", async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route   POST /users/auth
+// @desc    Authentication user and Login user
+// @access  Public
+router.post(
+	"/auth",
+	[
+        // validations for logging in
+		check("email", "Please include a valid email").isEmail(),
+		check("password", "Password is required").exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+            //displays errors
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+            // if no user
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid Credentials" }] });
+			}
+            // compares passwords 
+			const isMatch = await bcrypt.compare(password, user.password);
+            // if passwords don't match
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid Credentials" }] });
+			}
+
+			//Return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(payload, jwtSecret, { expiresIn: "5 days" }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
+);
 
     module.exports = router;
