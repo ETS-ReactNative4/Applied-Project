f02be331fa414219532cc91fa29ac28dba693702
Gareth O'Leary <G00375321@gmit.ie>
2021-11-29 16:39:11 +0000

    Backend Login system successfully created and tested through postman

--- AllergyScanner/backend/routes/auth.js
+++ AllergyScanner/backend/routes/auth.js
@@ -6,43 +6,43 @@ const User = require('../models/User')
 const bcrypt = require("bcryptjs");
 
 // Sign Up
-router.post('/signup', (req,res) => {
+router.post('/signup', (req, res) => {
     let { name, email, password } = req.body;
-    
+
     // removes whitespace
     name = name.trim();
     email = email.trim();
     password = password.trim();
-    
+
     // checks to see if variables are empty
-    if(name == "" || email == "" || password == ""){
+    if (name == "" || email == "" || password == "") {
         // returns a json object
         res.json({
             status: "FAILED",
             message: "Empty input fields!"
         });
         // check the format of name using regular expression
-    } else if (!/^[a-zA-Z]*$/.test(name)){
+    } else if (!/^[a-zA-Z]*$/.test(name)) {
         res.json({
             status: "FAILED",
             message: "Invalid name entered"
         });
         // check the format of email using regular expression
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
         res.json({
             status: "FAILED",
             message: "Invalid email entered"
         });
-       // checks the length of password 
-    } else if (password.length < 7){
+        // checks the length of password 
+    } else if (password.length < 7) {
         res.json({
             status: "FAILED",
             message: "Password is too short!"
         });
     } else {
         // checks if user already exists 
-        User.find({email}).then(result => {
-            if(result.length){
+        User.find({ email }).then(result => {
+            if (result.length) {
                 // a user already exists
                 res.json({
                     status: "FAILED",
@@ -90,6 +90,61 @@ router.post('/signup', (req,res) => {
 
 })
 
+// Sign In
+router.post('/signin', (req, res) => {
+    let { email, password } = req.body;
 
+    email = email.trim();
+    password = password.trim();
+    // check for empty variables
+    if (email == "" || password == "") {
+        // if empty, returns a message
+        res.json({
+            status: "FAILED",
+            message: "Credentials are empty!"
+        });
+    } else {
+        // check if user exists
+        User.find({ email })
+            .then(data => {
+                if (data.length) {
+                    // user exists
+                    const hashedPassword = data[0].password
+                    bcrypt.compare(password, hashedPassword).then(result => {
+                        if (result) {
+                            // password match
+                            res.json({
+                                status: "SUCCESS",
+                                message: "Sign In successful",
+                                data: data,
+                            });
+                        } else {
+                            res.json({
+                                status: "FAILED",
+                                message: "Invalid password entered!"
+                            });
+                        }
+                    }).catch(err => {
+                        res.json({
+                            status: "FAILED",
+                            message: "An error occurred while comparing passwords!"
+                        });
+                    })
+                } else {
+                    res.json({
+                        status: "FAILED",
+                        message: "Invalid credentials"
+                    });
+                }
+
+            }).catch(err => {
+                res.json({
+                    status: "FAILED",
+                    message: "An error occurred while checking user!"
+                });
+            })
+    }
+
+})
 
 module.exports = router;
