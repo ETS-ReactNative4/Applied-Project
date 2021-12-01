f54c959851dcb474a29d45ea2b0451cef054ed06
Gareth O'Leary <G00375321@gmit.ie>
2021-12-01 11:33:18 +0000

    Created two input boxes on Login page

--- AllergyScanner/frontend/components/Styles.js
+++ AllergyScanner/frontend/components/Styles.js
@@ -1,4 +1,4 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TextInput} from 'react-native';
 import Constants from 'expo-constants'
 import styled from 'styled-components/native';
 
@@ -8,6 +8,7 @@ export const StyledContainer = styled.View`
 flex: 1;
 padding: 25px;
 padding-top: ${StatusBarHeight + 30}px;
+background-color: #C9DFEC
 `
 
 export const InnerContainer = styled.View`
@@ -17,7 +18,7 @@ align-items: center;
 `
 
 export const PageLogo = styled.Image`
-width: 250px;
+width: 300px;
 height: 200px;
 `
 
@@ -26,4 +27,30 @@ font-size: 30px;
 text-align: center;
 font-weight: bold;
 padding: 10px;
+`
+
+export const SubTitle = styled.Text`
+font-size: 18px;
+margin-bottom: 20px;
+font-weight: bold;
+letter-spacing: 1px;
+`
+
+export const StyledFormArea = styled.View`
+width: 90%;
+`
+
+export const StyledTextInput = styled.TextInput`
+
+
+border-radius: 5px;
+font-size: 16px;
+height: 60px;
+margin-vertical: 3px;
+margin-bottom: 10px;
+`
+
+export const StyledInputLabel = styled.Text`
+font-size: 13px;
+text-align: left;
 `
--- AllergyScanner/frontend/screens/Login.js
+++ AllergyScanner/frontend/screens/Login.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import { StyledContainer, InnerContainer, PageLogo, PageTitle} from '../components/Styles';
+import { StyledContainer, InnerContainer, PageLogo, PageTitle, SubTitle, StyledFormArea,
+    StyledTextInput, StyledInputLabel} from '../components/Styles';
+import {StatusBar} from 'expo-status-bar';
+import {Formik} from 'formik'
+import {View} from 'react-native'
 
 const Login = () => {
     return (
     <StyledContainer>
+        <StatusBar style="dark"/>
         <InnerContainer>
             <PageLogo resizeMode="cover" source={require('../../assets/allergens.jpg')}/>
             <PageTitle>Allergy Scanner</PageTitle>
+            <SubTitle>Account Login</SubTitle>
+            <Formik
+            // Provide email and password values
+            initialValues={{email: '', password: ''}}
+            // on submit property that takes in values parameter
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+            >
+            {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+                <MyTextInput
+                      label="Email Address"
+                      placeholder="email"
+                      onChangeText={handleChange('email')}
+                      onBlur={handleBlur('email')}
+                      value={values.email}
+                      keyboardType="email-address"
+                      />
+
+                <MyTextInput
+                      label="Password"
+                      placeholder="* * * * * * * *"
+                      onChangeText={handleChange('password')}
+                      onBlur={handleBlur('password')}
+                      value={values.password}
+                      
+                      />
+
+            </StyledFormArea>)}
+            </Formik>
         </InnerContainer>
     </StyledContainer>
 
     );
 }
+
+// create input field component
+const MyTextInput = ({label,...props}) => {
+    return(
+        <View>
+<StyledInputLabel>{label}</StyledInputLabel>
+<StyledTextInput {...props}/>
+
+        </View>
+    )
+}
+
 
 export default Login;
 
