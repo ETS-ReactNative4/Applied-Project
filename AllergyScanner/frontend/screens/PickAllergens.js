7aa317836ca356691344abb2572c989aafb6897a
Gareth O'Leary <G00375321@gmit.ie>
2021-12-14 19:26:05 +0000

    Implemented allergen selection buttons

--- AllergyScanner/frontend/screens/PickAllergens.js
+++ AllergyScanner/frontend/screens/PickAllergens.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, StatusBar } from 'react-native';
-
 
 const PickAllergens = () => {
     allergens = [
@@ -15,27 +14,64 @@ const PickAllergens = () => {
         { allergen: 'Celery' },
     ]
 
-    return (
-        <View style={styles.container}>
-            {
-                allergens.map((data, index) => {
-                    return (
-                        <TouchableOpacity>
-                        <View style={styles.radioButton} key={index}>
-                            <Text style={{ color: 'black' }}>{data.allergen}</Text>
-                        </View>
-                        </TouchableOpacity>
-                    )
-                } )
+    const [data, setData] = useState(allergens);
+
+    useEffect(() => {
+
+        allergens.map((data, index) => {
+            data.isSelected = false;
+            return { ...data };
+
+        })
+        setData(data);
+        console.log('array data ==> ', data);
+    }
+    )
+    const selectionHandler = (ind) => {
+        const array = data.map((data, index) => {
+            if (ind == index) {
+                data.isSelected = !data.isSelected;
             }
+            return { ...data }
+
+        })
+        setData(array);
+        console.log("selection handler ==> ", array);
+    }
+
+    return (
+        <View style={styles.container} >
+            {
+                data.map((data, index) => {
+
+                    return (
+                        <View style={styles.radioView} key={index}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    selectionHandler(index)
+                                }}
+                            >
+                                <View style={[styles.radioButton, {
+                                    backgroundColor: data.isSelected ? '#aee0a8' : 'white',
+                                    borderColor: data.isSelected ? '#15b502' : 'grey'
+                                }]} >
+                                    <Text style={{ color: 'black' }}>{data.allergen}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )
+
+                })
+            }
+
         </View>
     )
-
 }
+
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
+        flexDirection: "row",
         backgroundColor: 'lightgrey',
         flex: 1,
         flexWrap: "wrap",
