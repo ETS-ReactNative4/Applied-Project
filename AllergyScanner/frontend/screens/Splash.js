6dd524ab90ef4f31fa78debe461a58bf0e65d0c0
Gareth O'Leary <G00375321@gmit.ie>
2022-03-21 21:30:24 +0000

    Added image and navigation to splash screen

--- AllergyScanner/frontend/screens/Splash.js
+++ AllergyScanner/frontend/screens/Splash.js
@@ -1,33 +1,108 @@
 import React from 'react';
 import { 
     View, 
-    Text,StyleSheet, StatusBar} from 'react-native';
+    Text, 
+    TouchableOpacity, 
+    Dimensions,
+    StyleSheet,
+    StatusBar,
+    Image,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {LinearGradient} from 'expo-linear-gradient';
 
-
-const Splash = () => {
+const Splash = ({navigation}) => {
     
-
     return (
-        <View style={styles.container}>
-        <StatusBar backgroundColor='#009387' barStyle="light-content"/>
-      <View style={styles.header}>
-          <Text>Splash Screen</Text>
-          </View>
-          </View>
+      <View style={styles.container}>
+          <StatusBar backgroundColor='#009387' barStyle="light-content"/>
+        <View style={styles.header}>
+            <Image 
+                
+            source={require('../../assets/Food.jpg')}
+            style={styles.logo}
+            resizeMode="stretch"
+            />
+        </View>
+        <View 
+            style={styles.footer}>
+            <Text style={[styles.title, {
+            }]}>Allergy Scanner</Text>
+            <Text style={styles.text}>Find out if your food is safe to eat.</Text>
+            <View style={styles.button}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
+            <LinearGradient
+                    colors={['#3b4850', '#41474b']}
+                    style={styles.signIn}
+                >
+                    <Text style={styles.textSign}>Get Started</Text>
+                    <MaterialIcons 
+                        name="navigate-next"
+                        color="#fff"
+                        size={20}
+                    />
+               </LinearGradient>
+            </TouchableOpacity>
+            </View>
+        </View>
+      </View>
     );
 };
 
 export default Splash;
 
+const {height} = Dimensions.get("screen");
+const height_logo = height * 0.28;
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        
-      },
-      header: {
-          flex: 2,
-          justifyContent: 'center',
-          alignItems: 'center'
-      },
-  });
+  container: {
+    flex: 1, 
+    backgroundColor: '#344955'
+  },
+  header: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  footer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingVertical: 50,
+      paddingHorizontal: 30
+  },
+  logo: {
+      width: height_logo,
+      height: height_logo,
+      borderRadius: 50
+  },
+  title: {
+      color: '#05375a',
+      fontSize: 30,
+      fontWeight: 'bold'
+  },
+  text: {
+      color: 'grey',
+      marginTop:5
+  },
+  button: {
+      alignItems: 'flex-end',
+      marginTop: 30
+  },
+  signIn: {
+      width: 150,
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 20,
+      flexDirection: 'row',
+      
+      
+  },
+  textSign: {
+      color: 'white',
+      fontWeight: 'bold'
+  }
+});
 
